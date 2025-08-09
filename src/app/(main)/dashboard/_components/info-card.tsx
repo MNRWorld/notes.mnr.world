fa586467c4a3f72639317cfd37e0552bd7878851
FrostFoe <frostfoe@gmail.com>
2025-08-09 09:16:56 +0000

    Make the whole web app fully mobile first each and every pixel must be o

--- src/app/(main)/dashboard/_components/info-card.tsx
+++ src/app/(main)/dashboard/_components/info-card.tsx
@@ -41,7 +41,7 @@ function InfoCard({
           <Icon className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="text-xl md:text-2xl font-bold">{content}</div>
+          <div className="text-xl font-bold md:text-2xl">{content}</div>
           {footer && <p className="text-xs text-muted-foreground">{footer}</p>}
         </CardContent>
       </Card>
